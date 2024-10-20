ddeb6339ba6239120a9d78d3239f1b411df25ad9
frecklehead <prashamsa35np@gmail.com>
2024-10-20 13:30:06 +0545

    11

--- navc/src/firebase.js
+++ navc/src/firebase.js
@@ -2,23 +2,25 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdVrszffTUjs-xi1kNex4wpSTjULR7jak",
-  authDomain: "project1-b4162.firebaseapp.com",
-  projectId: "project1-b4162",
-  storageBucket: "project1-b4162.appspot.com",
-  messagingSenderId: "551144010276",
-  appId: "1:551144010276:web:dd2dcc606a70fe5ad81eb1",
-  measurementId: "G-PRDX3C0XDN"
+  apiKey: "AIzaSyDZba78_O69v9ZtvYfNTUz4os2l8XUojqU",
+  authDomain: "project-1-928cb.firebaseapp.com",
+  projectId: "project-1-928cb",
+  storageBucket: "project-1-928cb.appspot.com",
+  messagingSenderId: "571198985976",
+  appId: "1:571198985976:web:68a308431145512609cd0d",
+  measurementId: "G-GL1ZG2MSS4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export const db=getFirestore(app);
 export default app;
